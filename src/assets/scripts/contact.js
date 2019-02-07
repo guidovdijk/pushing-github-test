7376c1b9aa3fe0dcbe9578dc28ffa5b9db5d6f7e
Guido van Dijk <guido.van.dijk@thevalley.nl>
2019-02-07 09:32:07 +0100

    Removed old files

--- src/assets/scripts/contact.js
+++
@@ -1,7 +0,0 @@
-export const testingB = 'hoi uit contact variable';
-export function testingWEG(){
-    return 'hoi uit contact, weg';
-} 
-export function testing(){
-    return 'hoi uit contact functie';
-} 
